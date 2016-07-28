b1e17b757bc0990f170be1c77d847230617cd630
Hugh Mooney <hugh.mooney@gmail.com>
2016-07-28 21:00:32 +0100

    Added simple input loop

--- main.py
+++ main.py
@@ -1,9 +1,11 @@
-#! /usr/bin/env python
+#!/usr/bin/env python
 
+import sys
 import os
+import fcntl
+import time
 import random
 import time
-import RPi.GPIO as GPIO
 import alsaaudio
 import wave
 import random
@@ -11,7 +13,6 @@ from creds import *
 import requests
 import json
 import re
-import sys
 from select import select
 from memcache import Client
 
@@ -27,111 +28,120 @@ mc = Client(servers, debug=1)
 path = os.path.realpath(__file__).rstrip(os.path.basename(__file__))
 
 
-
 def internet_on():
     print "Checking Internet Connection"
     try:
-        r =requests.get('https://api.amazon.com/auth/o2/token')
-	print "Connection OK"
+        r = requests.get('https://api.amazon.com/auth/o2/token')
+        print "Connection OK"
         return True
     except:
-	print "Connection Failed"
-    	return False
+        print "Connection Failed"
+        return False
 
-	
+
 def gettoken():
-	token = mc.get("access_token")
-	refresh = refresh_token
-	if token:
-		return token
-	elif refresh:
-		payload = {"client_id" : Client_ID, "client_secret" : Client_Secret, "refresh_token" : refresh, "grant_type" : "refresh_token", }
-		url = "https://api.amazon.com/auth/o2/token"
-		r = requests.post(url, data = payload)
-		resp = json.loads(r.text)
-		mc.set("access_token", resp['access_token'], 3570)
-		return resp['access_token']
-	else:
-		return False
-		
+    token = mc.get("access_token")
+    refresh = refresh_token
+    if token:
+        return token
+    elif refresh:
+        payload = {"client_id" : Client_ID, "client_secret" : Client_Secret, "refresh_token" : refresh, "grant_type" : "refresh_token", }
+        url = "https://api.amazon.com/auth/o2/token"
+        r = requests.post(url, data = payload)
+        resp = json.loads(r.text)
+        mc.set("access_token", resp['access_token'], 3570)
+        return resp['access_token']
+    else:
+        return False
+
 
 def alexa():
-	url = 'https://access-alexa-na.amazon.com/v1/avs/speechrecognizer/recognize'
-	headers = {'Authorization' : 'Bearer %s' % gettoken()}
-	d = {
-   		"messageHeader": {
-       		"deviceContext": [
-           		{
-               		"name": "playbackState",
-               		"namespace": "AudioPlayer",
-               		"payload": {
-                   		"streamId": "",
-        			   	"offsetInMilliseconds": "0",
-                   		"playerActivity": "IDLE"
-               		}
-           		}
-       		]
-		},
-   		"messageBody": {
-       		"profile": "alexa-close-talk",
-       		"locale": "en-us",
-       		"format": "audio/L16; rate=16000; channels=1"
-   		}
-	}
-	with open(path+'recording.wav') as inf:
-		files = [
-				('file', ('request', json.dumps(d), 'application/json; charset=UTF-8')),
-				('file', ('audio', inf, 'audio/L16; rate=16000; channels=1'))
-				]	
-		r = requests.post(url, headers=headers, files=files)
-	if r.status_code == 200:
-		for v in r.headers['content-type'].split(";"):
-			if re.match('.*boundary.*', v):
-				boundary =  v.split("=")[1]
-		data = r.content.split(boundary)
-		for d in data:
-			if (len(d) >= 1024):
-				audio = d.split('\r\n\r\n')[1].rstrip('--')
-		with open(path+"response.mp3", 'wb') as f:
-			f.write(audio)
+    url = 'https://access-alexa-na.amazon.com/v1/avs/speechrecognizer/recognize'
+    headers = {'Authorization' : 'Bearer %s' % gettoken()}
+    d = {
+        "messageHeader": {
+            "deviceContext": [
+                {
+                    "name": "playbackState",
+                    "namespace": "AudioPlayer",
+                    "payload": {
+                        "streamId": "",
+                        "offsetInMilliseconds": "0",
+                        "playerActivity": "IDLE"
+                    }
+                }
+            ]
+        },
+        "messageBody": {
+            "profile": "alexa-close-talk",
+            "locale": "en-us",
+            "format": "audio/L16; rate=16000; channels=1"
+        }
+    }
+    with open(path+'recording.wav') as inf:
+        files = [
+                ('file', ('request', json.dumps(d), 'application/json; charset=UTF-8')),
+                ('file', ('audio', inf, 'audio/L16; rate=16000; channels=1'))
+                ]
+        r = requests.post(url, headers=headers, files=files)
+    if r.status_code == 200:
+        for v in r.headers['content-type'].split(";"):
+            if re.match('.*boundary.*', v):
+                boundary =  v.split("=")[1]
+        data = r.content.split(boundary)
+        for d in data:
+            if (len(d) >= 1024):
+                audio = d.split('\r\n\r\n')[1].rstrip('--')
+        with open(path+"response.mp3", 'wb') as f:
+            f.write(audio)
 
-		os.system('mpg123 -q {}1sec.mp3 {}response.mp3 {}1sec.mp3'.format(path, path, path))
-	else:
-		for x in range(0, 3):
-			time.sleep(.2)
+        os.system('mpg123 -q {}1sec.mp3 {}response.mp3 {}1sec.mp3'.format(path, path, path))
+    else:
+        for x in range(0, 3):
+            time.sleep(.2)
 
-		
+
 
 
 
 def start():
-	timeout = 0.5
-	while True:
-		raw_input("Press Enter to start using Alexa")
-		inp = alsaaudio.PCM(alsaaudio.PCM_CAPTURE, alsaaudio.PCM_NORMAL, device)
-		inp.setchannels(1)
-		inp.setrate(16000)
-		inp.setformat(alsaaudio.PCM_FORMAT_S16_LE)
-		inp.setperiodsize(500)
-		audio = ""
-		print "Hit any key to stop recording"
-		rlist, _, _ = select([sys.stdin], [], [], timeout)
-		while (not rlist): # we keep recording while the button is pressed
-			l, data = inp.read()
-			if l:
-				audio += data
-			rlist, _, _ = select([sys.stdin], [], [], timeout)
-		rf = open(path+'recording.wav', 'w')
-		rf.write(audio)
-		rf.close()
-		inp = None
-		alexa()
+    while True:
 
-	
+        inp = alsaaudio.PCM(alsaaudio.PCM_CAPTURE, alsaaudio.PCM_NORMAL, device)
+        inp.setchannels(1)
+        inp.setrate(16000)
+        inp.setformat(alsaaudio.PCM_FORMAT_S16_LE)
+        inp.setperiodsize(500)
+        audio = ""
+        sleep_time = 0.2
+        fl = fcntl.fcntl(sys.stdin.fileno(), fcntl.F_GETFL)
+        fcntl.fcntl(sys.stdin.fileno(), fcntl.F_SETFL, fl | os.O_NONBLOCK)
+
+        print("Alexa is listening")
+        while True:
+            print("Hit ENTER key to stop recording")
+            try:
+                l, data = inp.read()
+                if l:
+                    audio += data
+                stdin = sys.stdin.read()
+                if "\n" in stdin or "\r" in stdin:
+                    break
+            except IOError:
+                pass
+            time.sleep(sleep_time)
+
+        rf = open(path+'recording.wav', 'w')
+        rf.write(audio)
+        rf.close()
+        inp = None
+        alexa()
+
+
 
 if __name__ == "__main__":
-	while internet_on() == False:
-		print "."
-	token = gettoken()
-	os.system('mpg123 -q {}1sec.mp3 {}hello.mp3'.format(path, path))
-	start()
+    while internet_on() == False:
+        print "."
+    token = gettoken()
+    os.system('mpg123 -q {}1sec.mp3 {}hello.mp3'.format(path, path))
+    start()
